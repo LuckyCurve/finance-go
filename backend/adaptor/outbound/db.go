aeab310c13eece1f19f76c75f05dc54407325917
LuckyCurve <luckycurvec@gmail.com>
2025-09-07 20:55:03 +0800

    refactor: 抽象基类, 使用绝对路径代替相对路径

--- backend/adaptor/outbound/db.go
+++ backend/adaptor/outbound/db.go
@@ -2,43 +2,73 @@ package outbound
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+type Base struct {
+	ID         uint `gorm:"column:id;type:int;primaryKey;autoIncrement"`
+	CreateTime time.Time
+	UpdateTime time.Time
+}
+
+func (b *Base) BeforeCreate(tx *gorm.DB) (err error) {
+	now := time.Now()
+	if b.CreateTime.IsZero() {
+		b.CreateTime = now
+	}
+	if b.UpdateTime.IsZero() {
+		b.UpdateTime = now
+	}
+	return
+}
+
+func (b *Base) BeforeUpdate(tx *gorm.DB) (err error) {
+	b.UpdateTime = time.Now()
+	return
+}
+
 type User struct {
-	ID   uint `gorm:"primaryKey"`
 	Name string
 	Age  int
+	Base
 }
 
 var db *gorm.DB
 
 func init() {
-
 	print("执行 init\n")
 	// 1. 连接 SQLite
 
-	db, err := gorm.Open(sqlite.Open("../data/test.db"), &gorm.Config{})
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic("get home path error")
+	}
+
+	db, err := gorm.Open(sqlite.Open(filepath.Join(home, ".finance-go", "test.db")), &gorm.Config{})
 	if err != nil {
 		fmt.Printf("failed to connect database, %v", err)
 	}
 
 	// 2. 自动迁移表结构
 	db.AutoMigrate(&User{})
-
-	// 3. 插入数据
-	db.Create(&User{Name: "Alice", Age: 25})
-
-	// 4. 查询数据
-	var user User
-	db.First(&user, "name = ?", "Alice")
-	fmt.Println("查询结果:", user)
-
-	// 5. 更新数据
-	db.Model(&user).Update("Age", 26)
-
-	// 6. 删除数据
-	db.Delete(&user)
 }
+
+// demo
+// // 3. 插入数据
+// db.Create(&User{Name: "Alice", Age: 25})
+
+// // 4. 查询数据
+// var user User
+// db.First(&user, "name = ?", "Alice")
+// fmt.Println("查询结果:", user)
+
+// // 5. 更新数据
+// db.Model(&user).Update("Age", 26)
+
+// // 6. 删除数据
+// db.Delete(&user)
